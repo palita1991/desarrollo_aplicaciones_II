ae8f07b1463ba70f3dd9ce04cb9fda6a1d46adbf
palita1991 <marianopalavecino7@gmail.com>
2026-09-08 19:46:34 -0300

    feat: navigation views

---
+++ Unidad II - Actividad II/AppProfile/AppProfile/Views/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using AppProfile.ViewModels;
+
+namespace AppProfile
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            BindingContext = new ProfileViewModel();
+        }
+        private void OnEdadTextChanged(object sender, TextChangedEventArgs e)
+        {
+            // Verificación que todos los caracteres sean numericos
+            bool esValido = e.NewTextValue.All(char.IsDigit);
+
+            if (!esValido)
+            {
+                // Al querer ingresar letras las borra automaticamente
+                ((Entry)sender).Text = e.OldTextValue;
+            }
+        }
+    }
+}
